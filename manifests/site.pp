9b177c6076eaeca9be23249963f21b3530487342
MattMaus <131156963+MattMaus@users.noreply.github.com>
2023-04-19 16:19:22 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'Bien le kikoo les assujetis !',
-    owner => 'root',
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
